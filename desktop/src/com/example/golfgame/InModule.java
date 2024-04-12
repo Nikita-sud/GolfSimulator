90fac1948de2c40369ebc39d4c81bee268f13f73
EliasF05 <eliasjohann05@outlook.de>
2024-04-12 22:21:20 +0200

    still trying

--- desktop/src/com/example/golfgame/InModule.java
+++ desktop/src/com/example/golfgame/InModule.java
@@ -1,5 +1,5 @@
 package com.example.golfgame;
-// Just testing out comitting ...
+// Just testing out comitting .
 class InModule{
     public static void main(String[] args) {
         System.out.println("hello world");
