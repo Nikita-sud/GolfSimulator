f2114d6821d2fac460ab068a7f0c466d5df0f3c1
Nichita Bulgaru <n.bulgaru@student.maastrichtuniversity.nl>
2024-04-26 16:39:14 +0200

    Big changes

--- core/src/com/example/golfgame/GolfGame.java
+++ core/src/com/example/golfgame/GolfGame.java
@@ -2,15 +2,18 @@ package com.example.golfgame;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class GolfGame extends Game {
     private Screen mainScreen;
     private Screen gameScreen;
+    private AssetManager assetManager;
 
     @Override
     public void create() {
+        assetManager = new AssetManager();
         mainScreen = new MainMenuScreen(this);
-        gameScreen = new GolfGameScreen(this);
+        gameScreen = new GolfGameScreen(this,assetManager);
         this.setScreen(mainScreen);
     }
 
