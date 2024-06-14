a484ee3329d23d8874ea2be3cdd1c95acf565b24
EliasF05 <eliasjohann05@outlook.de>
2024-06-14 15:25:48 +0200

    parrallel processing for simulator

--- core/src/com/example/golfgame/simulator/PhysicsSimulator.java
+++ core/src/com/example/golfgame/simulator/PhysicsSimulator.java
@@ -1,7 +1,13 @@
 package com.example.golfgame.simulator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import com.example.golfgame.physics.PhysicsEngine;
 import com.example.golfgame.physics.ODE.RungeKutta;
@@ -70,9 +76,46 @@ public class PhysicsSimulator {
             float velocityMagnitude = random.nextFloat(1, 5);
             float angle = random.nextFloat(0f, (float)(2*Math.PI));
             res[i] = hit(velocityMagnitude, angle);
+            System.out.println(res[i]);
         }
         return res;
     }
+
+     /**
+     * performs number of hit simulations within a certain unit radius of the goal
+     * @param n number of simulations
+     * @param goal target BallState
+     * @param radius search radius
+     * @return array of BallState results
+     */
+    public BallState[] parallelRandomHits(int n, BallState goal, float radius) throws InterruptedException, ExecutionException {
+        BallState[] res = new BallState[n];
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<BallState>> futures = new ArrayList<Future<BallState>>();
+
+        for (int i = 0; i < n; i++) {
+            futures.add(executor.submit(() -> {
+                float ballX = random.nextFloat() * (2 * radius) - radius;
+                float ballY = random.nextBoolean() ? (float) Math.sqrt(radius * radius - ballX * ballX) : -(float) Math.sqrt(radius * radius - ballX * ballX);
+                ballX += goal.getX();
+                ballY += goal.getY();
+                ball.setX(ballX);
+                ball.setY(ballY);
+                float velocityMagnitude = random.nextFloat() * (5 - 1) + 1;
+                float angle = random.nextFloat() * (2 * (float) Math.PI);
+                return hit(velocityMagnitude, angle);
+            }));
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = futures.get(i).get();
+            System.out.println(res[i]);
+        }
+
+        executor.shutdown();
+        return res;
+    }
+
 
 
     /**
@@ -86,12 +129,12 @@ public class PhysicsSimulator {
     }
 
     
-    public static void main(String[] args){
-        Function h = new Function("0.1x^2+0.1y^2+10", "x", "y");
+    public static void main(String[] args) throws InterruptedException, ExecutionException{
+        Function h = new Function("cos(0.3x)*sin(0.5y)+10", "x", "y");
         PhysicsSimulator sim = new PhysicsSimulator(h);
        
         
-        System.out.println(Arrays.toString(sim.randomHits(10, new BallState(0, 0, 0, 0), 10)));
+        System.out.println(Arrays.toString(sim.parallelRandomHits(100, new BallState(0, 0, 0, 0), 10)));
     }
 
     
